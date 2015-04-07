92223d20f130cb1d9d9faee421e5e95f14dd8468
ojgarciab <oscar.github@linaresdigital.com>
2015-04-07 16:47:30 +0200

    Fix 3: Code to initialize webcam device

--- v4l2.cpp
+++ v4l2.cpp
@@ -5,31 +5,57 @@
  *      Author: redstar
  */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include "v4l2.h"
 
 namespace v4l2 {
 
 Camera::Camera(const char *device, int weigth, int height, int fps = 10) {
   /* Camera not active yet */
-  this->active = false;
+  this->active_ = false;
   /* Save camera data */
-  this->device = device;
-  this->width = width;
-  this->height = height;
-  this->fps = fps;
+  this->device_ = device;
+  this->width_ = width_;
+  this->height_ = height;
+  this->fps_ = fps;
 }
 
 void Camera::Start() {
-  active = true;
+  struct stat st;
+  /* Get stat data from device file */
+  if (stat(device_, &st) == -1) {
+    throw std::string("Couldn't stat " << device_ << " file.");
+  }
+  /* Is a character special device file? */
+  if (!S_ISCHR(st.st_mode)) {
+    throw std::string("File " << device_ << " is not a device.");
+  }
+  /* Open device file */
+  camera_fd_ = open(device_, O_RDWR | O_NONBLOCK, 0);
+  if (camera_fd_ == -1) {
+    throw std::string(
+        "Can't open " << device_ << ": [" << errno << "] " << strerror(errno));
+  }
+  active_ = true;
 }
 
 void Camera::Stop() {
-  active = false;
+  active_ = false;
+}
+
+bool Camera::is_active() {
+  return active_;
 }
 
 Camera::~Camera() {
   /* Stop and close camera, free buffers, ect */
-  if (active == true) {
+  if (active_ == true) {
     Stop();
   }
 }
--- v4l2.h
+++ v4l2.h
@@ -8,26 +8,33 @@
 #ifndef JDEROBOT_COMPONENTS_V4L2SERVER_V4L2_H_
 #define JDEROBOT_COMPONENTS_V4L2SERVER_V4L2_H_
 
+#include <iostream>
+
 namespace v4l2 {
+
 class Camera {
  private:
   /* Camera device name */
-  const char *device;
+  const char *device_;
+  /* Camera file descriptor */
+  int camera_fd_;
   /* Image width */
-  int width;
+  int width_;
   /* Image height */
-  int height;
+  int height_;
   /* Frames per second requested */
-  int fps;
+  int fps_;
   /* Camera active (capturing frames) */
-  bool active;
+  bool active_;
 
  public:
   Camera(const char *device, int weight, int height, int fps = 10);
-  void Start();
+  void Start() throw (std::string);
   void Stop();
+  bool is_active();
   ~Camera();
 };
-}
+
+} /* namespace */
 
 #endif /* JDEROBOT_COMPONENTS_V4L2SERVER_V4L2_H_ */
