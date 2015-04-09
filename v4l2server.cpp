4ee2df6609c6d6ae16efbb3a191099c93a88e209
ojgarciab <oscar.github@linaresdigital.com>
2015-04-10 01:14:31 +0200

    Fix #9: Implement v4l2 start and stop video streaming

--- v4l2server.cpp
+++ v4l2server.cpp
@@ -9,11 +9,23 @@
 
 #include "v4l2.h"
 
+/** Interface class to define callback function */
+class FrameCallback {
+ public:
+  // Callback function called when new frame is fetched from camera
+  virtual int onFrame(void* buffer, int length) = 0;
+ protected:
+     ~FrameCallback(){}
+};
+
 int main(int argc, char** argv) {
-  v4l2::Camera camera = v4l2::Camera("/dev/video1", 320, 240, 10);
+  v4l2::Camera camera = v4l2::Camera("/dev/video0", 320, 240, 10);
   try {
     camera.Open();
     std::cout << "Get ready! -> " << camera.is_active() << std::endl;
+    camera.Start();
+    std::cout << "Camera started! -> " << camera.is_active() << std::endl;
+    camera.Stop();
   } catch (std::string& e) {
     std::cout << "ERROR: " << e << std::endl;
   }
